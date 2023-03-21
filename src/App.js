dc530def742238f1d205a01dae384690ada184f8
TattooColor <117419235+TattooColor@users.noreply.github.com>
2023-03-21 21:40:08 +0700

    test01

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './Header'; //Include Header
+import Footer from './Footer';
+import Shop from './Shop'
+function App() {
+  return (
+    <div className="App">
+      
+       
+          <Header></Header>
+           <Shop></Shop>
+          <Footer></Footer>
+        
+     
+    </div>
+  );
+}
+export default App;
